77622682cee2c01bf9cfbe744814ca42663fbac3
Julio <julio.brandao@upe.br>
2026-09-07 11:18:17 -0300

    feat: criada a classe Player com encapsulamento

---
+++ src/Player.java
@@ -0,0 +1,16 @@
+public class Player {
+    private String nome;
+    private char simbolo;
+
+    public Player(String nome, char simbolo) {
+        this.nome = nome;
+        this.simbolo = simbolo;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public char getSimbolo() {
+        return simbolo;
+    }
+
+}
